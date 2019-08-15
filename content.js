7748ed0040e802ec337c75ee6a28fead169247fb
Moshec1976 <45510925+Moshec1976@users.noreply.github.com>
2019-08-15 17:05:25 +0300

    Message app screen

---
+++ content.js
@@ -0,0 +1,29 @@
+let myBad = document.getElementById('timeText');
+let myName = ["Moshe",
+    "Chayon"
+];
+myBad.innerHTML = `12:00`;
+
+let myBad2 = document.getElementById('nameText');
+myBad2.innerHTML = `משה`;
+
+let messageContent = document.getElementById('messageText');
+messageContent.innerHTML = `בעלתי קוביית קרח ועדיין לא חירבנתי אותה החוצה!! מה אני עושה עכשיו? `
+
+let foolsTime2 = document.getElementById('timeText2');
+foolsTime2.innerHTML = `12:10`;
+
+let foolsName2 = document.getElementById('nameText2');
+foolsName2.innerHTML = `אמיר`;
+
+let foolsMessage2 = document.getElementById('messageText2');
+foolsMessage2.innerHTML = `אני לא יודע!!! תלך לבית חולים עכשיו. לעשה צילומים!!`;
+
+let timeHQ = document.getElementById('timeText3');
+timeHQ.innerHTML = `12:11`;
+
+let nameHQ = document.getElementById('nameText3');
+nameHQ.innerHTML = 'מורן';
+
+let messageHQ = document.getElementById('messageText3');
+messageHQ.innerHTML = `באמא שלי, אתם שתי מפגרים. זאת קןביית קרח!! היא כבר מנסה!!! `;
